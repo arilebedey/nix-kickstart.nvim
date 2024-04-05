070e3abf680703384438e2c33fdd19a82d915c96
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-04-05 19:20:28 +0200

    rel numbers off

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,7 +20,7 @@ end
 opt.path = vim.o.path .. '**'
 
 opt.number = true
-opt.relativenumber = true
+-- opt.relativenumber = true
 opt.cursorline = true
 opt.lazyredraw = true
 opt.showmatch = true -- Highlight matching parentheses, etc
